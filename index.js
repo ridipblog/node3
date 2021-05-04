79fa4f02550e4d77855e174f31fb7966f2357f8d
ridipblog <memorytemp5@gmail.com>
2021-05-04 08:58:05 -0700

    comment 3

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.get("/",(req,res,next)=>{
 app.get("/about",(req,res,next)=>{
 	res.send("About Page Welcome from ridip");
 });
+app.get("/contact",(req,res,next)=>{
+	res.send("Contact Page Welcome from ridip");
+});
+app.get("*",(req,res,next)=>{
+	res.send("Error Page");
+});
 app.listen(port,()=>{
 	console.log("Server runing");
 });
